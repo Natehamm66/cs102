92405f41f9bbd5191bc25cbebac5bf290f26a275
Nathan Hammond <natehamm66@gmail.com>
2014-01-31 20:43:25 -0500

    Exercise 6.1

---
+++ isDisvisible/src/net/nate/hw1/Main.java
@@ -0,0 +1,13 @@
+package net.nate.hw1;
+
+public class Main {
+	public static boolean isDivisible(boolean isTrue, int m, int n) {
+		if (n % m == 0)
+			isTrue = true;
+		return isTrue;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
