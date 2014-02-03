bfcb3af3d33471d54ed439c0cff342205644de3c
Nathan Hammond <natehamm66@gmail.com>
2014-02-03 09:52:54 -0500

    IT WORKS

--- isDisvisible/src/net/nate/hw1/Main.java
+++ isDisvisible/src/net/nate/hw1/Main.java
@@ -1,13 +1,15 @@
 package net.nate.hw1;
 
 public class Main {
-	public static boolean isDivisible(boolean isTrue, int m, int n) {
-		if (n % m == 0)
-			isTrue = true;
-		return isTrue;
-	}
 
+	public static boolean isDivisible(int m, int n) {
+		return (m % n == 0); 
+		}
 	public static void main(String[] args) {
-
+		if (isDivisible(6,3)) {
+		    System.out.println("6 is divisible by 3!");
+		} else {
+		    System.err.println("Ooops... 6 should be divisible by 3");
+		}
 	}
 }
