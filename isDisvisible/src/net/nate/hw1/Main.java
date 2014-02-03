2cc1a7ef3d4813e590fa4a33f77b72c8cdd12e7f
Greg Haines <gresrun@gmail.com>
2014-02-03 09:39:03 -0500

    One-liner
    http://i.imgur.com/YfclGhf.jpg

--- isDisvisible/src/net/nate/hw1/Main.java
+++ isDisvisible/src/net/nate/hw1/Main.java
@@ -1,10 +1,8 @@
 package net.nate.hw1;
 
 public class Main {
-	public static boolean isDivisible(boolean isTrue, int m, int n) {
-		if (n % m == 0)
-			isTrue = true;
-		return isTrue;
+	public static boolean isDivisible(int m, int n) {
+		return (m % n == 0);
 	}
 
 	public static void main(String[] args) {
