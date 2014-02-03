268b4efb014bec0f4b2f6760ea380d5fa95b85f4
Nathan Hammond <natehamm66@gmail.com>
2014-02-03 11:22:27 -0500

    IT WORKS TOO!

--- Can or Can't Triangle/src/net/nate/hw1/Main.java
+++ Can or Can't Triangle/src/net/nate/hw1/Main.java
@@ -1,9 +1,20 @@
 package net.nate.hw1;
+
 public class Main {
-	public static boolean isTriangle(int a,int b,int c){
-		if (c > a+b | a > c+b | b > c+a ){
+	public static boolean isTriangle(int a, int b, int c) {
+		if (c > a + b || a > c + b || b > c + a) {
+			return false;
+		} else {
+			return true;
 		}
-		return false;
+
+	}
+
+	public static void main(String[] args) {
+		if (isTriangle(7, 4, 3)) {
+			System.out.println("It is a triangle");
+		} else {
+			System.out.println("It is not a triangle");
+		}
 	}
 }
-
