b033fd6d8583d2971d258fb636aeba2f37e6096a
Greg Haines <gresrun@gmail.com>
2014-02-08 13:42:23 -0500

    One-liner

--- Can or Can't Triangle/src/net/nate/hw1/Main.java
+++ Can or Can't Triangle/src/net/nate/hw1/Main.java
@@ -2,12 +2,7 @@ package net.nate.hw1;
 
 public class Main {
 	public static boolean isTriangle(int a, int b, int c) {
-		if (c > a + b || a > c + b || b > c + a) {
-			return false;
-		} else {
-			return true;
-		}
-
+		return !(c > a + b || a > c + b || b > c + a);
 	}
 
 	public static void main(String[] args) {
